0a353ba7f269f30f3b43c80e52fb9f419e707754
Jay Prakash Bind <60916627+codejay411@users.noreply.github.com>
2020-09-06 21:56:22 +0530

    pattern 5 commit

---
+++ pattern5.py
@@ -0,0 +1,9 @@
+num=int(input("enter the number  : "))
+for i in range(0,num):
+    for j in range(0,i+1):
+        print("* ",end="")
+    print()
+for i in range(num-1,0,-1):
+    for j in range(0,i):
+        print("* ", end="")
+    print()
